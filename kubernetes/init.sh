08bd3b9b03f64113208605d7e91b04dfd6b4c036
aidan.mattson@smoothstack.com <aidan.mattson@wsu.edu>
2021-11-30 15:22:23 -0800

    Separation of multiple deployments for HTTP/HTTPS ingress

--- kubernetes/init.sh
+++ kubernetes/init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# initialize cluster and load secrets
+# recreate cluster
+kind delete cluster --name=api-cluster
 kind create cluster --config=clusterconfig.yaml
-kubectl apply -f secret.yaml
 
 # initialize ingress-nginx and clean up
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
@@ -15,6 +15,7 @@ kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
 # create and move to new namespace
 kubectl create namespace apis-ns
 kubens apis-ns
+kubectl apply -f secret.yaml
 
 # load objects
 cd objects && kubectl apply -f .
