f81ee931bf6928e2c8c73377197d3041b34f431b
aidan.mattson@smoothstack.com <aidan.mattson@wsu.edu>
2021-11-30 13:08:09 -0800

    UTOPIA-64: Kubernetes Local Cluster - added working networking setup

---
+++ kubernetes/init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# recreate cluster
+kind delete cluster --name=api-cluster
+kind create cluster --config=clusterconfig.yaml
+
+# initialize ingress-nginx and clean up
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
+kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
+
+# create and move to new namespace
+kubectl create namespace apis-ns
+kubens apis-ns
+kubectl apply -f secret.yaml
+
+# load objects
+cd objects && kubectl apply -f .
