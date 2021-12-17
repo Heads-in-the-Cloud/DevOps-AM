be533223cca9835a7466be8d73c77f54c27d27bc
aidan.mattson@smoothstack.com <aidan.mattson@wsu.edu>
2021-12-16 20:02:44 -0800

    Added EKS kubernetes tools

---
+++ kubernetes/eks-init.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+(
+  # create basic ingress framework
+  cd init
+  kubectl apply -f ns-sa.yaml
+  kubens nginx-ingress
+  kubectl apply -f default-server-secret.yaml
+
+  # create ingress custom configs
+  cd ../eks-custom
+  kubectl apply -f .
+
+  # create ingress control items
+  cd ../eks-config
+  kubectl apply -f .
+
+  # wait for ingress to finish
+  kubectl wait --namespace nginx-ingress \
+    --for=condition=ready pod \
+    --selector=app.component=controller \
+    --timeout=180s
+)
+
+# create and move to new namespace
+kubectl create ns apis-ns
+kubens apis-ns
+kubectl apply -f secret.yaml
+
+# load objects
+cd objects && kubectl apply -f .
+
+# print eks endpoint
+kubectl get svc --namespace=nginx-ingress | awk 'NR==2{print $4}'
