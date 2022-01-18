7c648d5342f85c52777c7c1e055392058226d6f5
aidan.mattson@smoothstack.com <aidan.mattson@wsu.edu>
2022-01-18 13:11:35 -0800

    modified local ECS Jenkinsfile

--- terraform/output.tf
+++ terraform/output.tf
@@ -6,11 +6,3 @@ output "AWS_VPC_ID" {
 output "AWS_RDS_ENDPOINT" {
   value = module.utopia-db.db_address
 }
-
-output "AWS_RDS_USERNAME" {
-  value = nonsensitive(local.db_creds.DB_USERNAME)
-}
-
-output "AWS_RDS_PASSWORD" {
-  value = nonsensitive(local.db_creds.DB_PASSWORD)
-}
