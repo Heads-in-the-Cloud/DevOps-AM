f9bacc7d53d5ed5c2a38cd77e99e51b18074efcf
aidan.mattson@smoothstack.com <aidan.mattson@wsu.edu>
2022-01-18 12:26:25 -0800

    fixed sensitivity error

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,12 +5,15 @@ output "AWS_VPC_ID" {
 
 output "AWS_RDS_ENDPOINT" {
   value = module.utopia-db.db_address
+  sensitive = true
 }
 
 output "AWS_RDS_USERNAME" {
   value = local.db_creds.DB_USERNAME
+  sensitive = true
 }
 
 output "AWS_RDS_PASSWORD" {
   value = local.db_creds.DB_PASSWORD
+  sensitive = true
 }
