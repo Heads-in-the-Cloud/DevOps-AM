d6977a1c17a2b4282fbc460962229a89100a2170
aidan.mattson@smoothstack.com <aidan.mattson@wsu.edu>
2022-01-20 15:03:53 -0800

    modifying deploy options

--- terraform/modules/ecs/tasks.tf
+++ terraform/modules/ecs/tasks.tf
@@ -3,44 +3,44 @@
 # ECS Task Info #
 #################
 
-resource "aws_ecs_task_definition" "api_tasks" {
-  // general
-  for_each = local.indexes
-  family = "AM-task-${each.key}"
-  requires_compatibilities = ["FARGATE"]
-
-  // variable
-  task_role_arn = var.task_arn
-  execution_role_arn = var.task_arn
-  network_mode = var.net_mode
-  memory = var.memory
-  cpu = var.cpu
-
-  // containers
-  container_definitions = jsonencode([
-    {
-      name = "AM-task-${each.key}"
-      image = local.repos[each.key]
-      memory = var.memory
-      cpu = var.cpu
-      essential = true
-      portMappings = [
-        {
-          containerPort = each.value
-          hostPort = each.value
-        }]
-      environment = var.environment
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          awslogs-group = "/ecs/AM-ecs-apis"
-          awslogs-region = "us-west-2"
-          awslogs-stream-prefix = "ecs"
-        }
-      }
-    }
-  ])
-}
+//resource "aws_ecs_task_definition" "api_tasks" {
+//  // general
+//  for_each = local.indexes
+//  family = "AM-task-${each.key}"
+//  requires_compatibilities = ["FARGATE"]
+//
+//  // variable
+//  task_role_arn = var.task_arn
+//  execution_role_arn = var.task_arn
+//  network_mode = var.net_mode
+//  memory = var.memory
+//  cpu = var.cpu
+//
+//  // containers
+//  container_definitions = jsonencode([
+//    {
+//      name = "AM-task-${each.key}"
+//      image = local.repos[each.key]
+//      memory = var.memory
+//      cpu = var.cpu
+//      essential = true
+//      portMappings = [
+//        {
+//          containerPort = each.value
+//          hostPort = each.value
+//        }]
+//      environment = var.environment
+//      logConfiguration = {
+//        logDriver = "awslogs"
+//        options = {
+//          awslogs-group = "/ecs/AM-ecs-apis"
+//          awslogs-region = "us-west-2"
+//          awslogs-stream-prefix = "ecs"
+//        }
+//      }
+//    }
+//  ])
+//}
 //
 //resource "aws_ecs_service" "api_services" {
 //  for_each = local.indexes
