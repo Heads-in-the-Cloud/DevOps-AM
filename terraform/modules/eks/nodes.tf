bea8502c33988a389793242d10956f264fe6e4e1
aidan.mattson@smoothstack.com <aidan.mattson@wsu.edu>
2022-01-21 13:57:35 -0800

    Added resource dependencies and upgrades EKS nodes

--- terraform/modules/eks/nodes.tf
+++ terraform/modules/eks/nodes.tf
@@ -9,6 +9,8 @@ resource "aws_eks_node_group" "eks_nodes" {
   node_role_arn = aws_iam_role.EKS_nodegroup_role.arn
   subnet_ids = var.eks_subnets
 
+  instance_types = ["t3.large"]
+
   scaling_config {
     desired_size = 2
     max_size = 4
