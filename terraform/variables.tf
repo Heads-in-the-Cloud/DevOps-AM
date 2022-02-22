fd06dfe3653d342ce1a85fee9fae932a91fc400b
aidan.mattson@smoothstack.com <aidan.mattson@wsu.edu>
2022-02-22 13:13:20 -0800

    abstracted AWS login to profiles

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,14 +11,6 @@ variable "REGION_ID" {
   type = string
 }
 
-variable "AWS_ACCESS_KEY" {
-  type = string
-}
-
-variable "AWS_SECRET_KEY" {
-  type = string
-}
-
 variable "AWS_SECRET_ID" {
   type = string
 }
