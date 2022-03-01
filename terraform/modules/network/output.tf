6e9eeb5d4acc14ea7255ba7aef66b0b738651319
aidan.mattson@smoothstack.com <aidan.mattson@wsu.edu>
2022-03-01 15:52:42 -0800

    [HOTFIX] Updated README with new directory changes

--- terraform/modules/network/output.tf
+++ terraform/modules/network/output.tf
@@ -3,24 +3,24 @@
 # Variables for outside use #
 #############################
 
-// private subnet group for database use
+// Private subnet group ID for database use
 output "subnet_group_id" {
   value = aws_db_subnet_group.subnet_group_private.id
 }
 
-
-
+// List of private subnets for passing
 output "private_subnets" {
   value = [ aws_subnet.private_subnet_1.id,
             aws_subnet.private_subnet_2.id ]
 }
 
+// List of public subnets for passing
 output "public_subnets" {
   value = [ aws_subnet.public_subnet_1.id,
             aws_subnet.public_subnet_2.id ]
 }
 
-// vpc to use across the program
+// VPC ID to use across the program
 output "utopia_vpc" {
   value = aws_vpc.am-vpc-db.id
 }
