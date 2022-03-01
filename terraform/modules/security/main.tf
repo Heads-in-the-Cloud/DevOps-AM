2535f66744824cab7f1737ac5219c76864ed4eee
aidan.mattson@smoothstack.com <aidan.mattson@wsu.edu>
2022-03-01 14:30:58 -0800

    Consolidating Security Groups 1

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -43,13 +43,6 @@ resource "aws_security_group" "db_security" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Name = "${var.environment_name}-rds-security"
   }
@@ -122,14 +115,6 @@ resource "aws_security_group" "eks_api_access" {
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # SSH
-  ingress {
-    from_port   = 22
-    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
